95306b9cab70fba64f2dce51c897185e206141a9
NicolasDeza <nicolasdeza@hotmail.be>
2024-03-11 21:43:30 +0100

    Creation traitement

--- contact.php
+++ contact.php
@@ -1,9 +1,45 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] === "POST")
-{
-    if (empty($POST["prenom"])) {
-        echo "Le champs prénom est requis";
+ // Traitement du formulaire
+if($_SERVER["REQUEST_METHOD"] === "POST") {
+    
+    // Creation tableau pour messages d'erreurs
+    $erreurs = [];
+
+    $nom = htmlentities($_POST["nom"]);
+    $prenom = htmlentities($_POST["prenom"]);
+    $email = htmlentities($_POST["email"]);
+    $message = htmlentities($_POST["message"]);
+
+     // Nom
+    if(isset($_POST["nom"]) && !empty($_POST["nom"]))
+    {
+        if(strlen($nom) < 10 || strlen($nom) > 255) {
+            $erreurs = "Le nom ne peut contenir qu'entre 2 et 255 caractères";
+       }
     }
+    // Prenom
+    if(isset($_POST["prenom"]) && !empty($_POST["prenom"]))
+    {
+        if(strlen($prenom) < 10 || strlen($prenom) > 255) {
+            $erreurs = "Le nom ne peut contenir qu'entre 2 et 255 caractères";
+       }
+    }
+    // Email
+    if(isset($POST["email"]) && !empty($POST["email"])) {
+
+    }
+    // Message 
+    if(isset($POST["message"]) && !empty($POST["message"])) {
+        if(strlen($message) < 10 || strlen($message) > 3000) {
+            $erreurs = "Le message ne peut contenir qu'entre 10 et 3000 caractères";
+        }
+    }
+    // Condition pour afficher ou pas les erreurs
+    if(!empty($erreurs)) {
+    foreach ($erreurs as $erreur) {
+        echo "<p>$erreur</p>";
+    }
+  }
 }
 
 // Valeur spécifique de la page
@@ -12,7 +48,8 @@ $metaDescription ="Description page contact";
 
 ?>
 
-<?php
+
+<?php // Ajout du header
 require_once (__DIR__ . DIRECTORY_SEPARATOR . "header.php") 
 ?>
 
@@ -24,7 +61,7 @@ require_once (__DIR__ . DIRECTORY_SEPARATOR . "header.php")
         <input type="text" id="nom" name="nom" minlength="2" maxlength="255"  required>
     <br><br>
     <label for="prenom">Prénom :<label>
-        <input type="text" id="prenom" name="prenom" minlength="2" maxlength="255">
+        <input type="text" id="prenom" name="prenom" minlength="2" maxlength="255" required>
     <br><br>
     <label for="email">Email :</label>
         <input type="email" id="email" name="email" required>
